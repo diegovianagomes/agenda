58f36832a3aef51d116c5aa0af96d91eba0fbcda
dvgba <diegovianagomes01@gmail.com>
2025-03-01 09:05:01 -0300

    🚧 chore: add linting and formatting configuration with Husky and Prettier (AG-00)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
+/* Segunda possibilidade para  adicionar o o plugin Prettier , createRequire cria uma função similar ao 'require' que pode ser usada em módulos ESM
+import { createRequire } from 'module';*/
 import nx from '@nx/eslint-plugin';
+// adicionei o plugin prettier para que o eslint possa formatar o código
+import prettier from 'eslint-plugin-prettier/recommended';
+
+/* import.meta.url determina o caminho do módulo e auxilia na resolução do módulo requerido
+const require = createRequire(import.meta.url);
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');*/
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  prettier,
   {
     ignores: ['**/dist'],
   },
